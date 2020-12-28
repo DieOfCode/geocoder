63a6a4064ccb4e2542ee1af2081dbcc4f3f3af28
DieOfCode <fr0ststone878@gmail.com>
2020-12-28 15:13:57 +0500

    update CLI

--- geo/CLI.py
+++ geo/CLI.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename="geo.log", filemode="w",
 @dataclass
 class CLI:
     _status: str = "start"
-    raw_address: str = ""
+    raw_data: str = ""
     working: bool = True
 
     def get_answer(self) -> str:
@@ -21,33 +21,41 @@ class CLI:
             if self._status == "start":
                 print("Введи адресс, получи более интересную информацию \n"
                       "Формат ввода адресса: Город Улица Номер дома\n"
-                      "Чтобы прекратить работу введите конец или \\end \n""")
-                self.raw_address = input()
-                if self.raw_address.lower() == "\\end":
+                      "Чтобы прекратить работу введите конец или \\end \n"
+                      "Для получения краткой информации введите \\help \n")
+                self.raw_data = input()
+                if self.raw_data.lower() == "\\end":
                     break
+                if self.raw_data.lower() == "\\help":
+                    print("Простое приложение для получения гео данных "
+                          "объекта\n"
+                          "Для получения необходимой информации введите\n"
+                          "Данные об объекте в фромате Город Улица Номер "
+                          "дома \n")
+                    self._status= "start"
                 self._status = "wait_answer"
             if self._status == "wait_answer":
-                if self.raw_address == "":
+                if self.raw_data == "":
                     print(
                         "Ну, ты подумай, не торопись. "
                         "И введи адрес. Не надо нам тут баловаться")
                     self._status = "start"
 
                 try:
-                    geocoder = Geocoder(self.raw_address)
+                    geocoder = Geocoder(self.raw_data)
                     yield geocoder.find_geo_data()
                     self._status = "start"
                 except NoGeocoderDataException as geocoder_data_exception:
                     geocoder_data_exception.exception_handling(
-                        self.raw_address)
+                        self.raw_data)
                 except UnknownCity as city_exception:
-                    city_exception.exception_handling(self.raw_address)
+                    city_exception.exception_handling(self.raw_data)
                 except UnknownAddress as address_exception:
-                    address_exception.exception_handling(self.raw_address)
+                    address_exception.exception_handling(self.raw_data)
                 except Exception as another_exception:
                     print("Что-то пошло совсем не так")
                     logging.exception("address:{} ,{} {}"
-                                      .format(self.raw_address, "АЛЯРМ!!!!!",
+                                      .format(self.raw_data, "АЛЯРМ!!!!!",
                                               another_exception.__str__()))
                 finally:
                     self._status = "start"
